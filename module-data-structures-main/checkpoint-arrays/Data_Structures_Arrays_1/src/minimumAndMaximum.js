b90baf4c8661199c97226043c24db6d615427b4d
Dakota Watkins <dakota.watkins007@gmail.com>
2022-01-07 15:26:38 -0800

    updated arrays checkpoint

--- module-data-structures-main/checkpoint-arrays/Data_Structures_Arrays_1/src/minimumAndMaximum.js
+++ module-data-structures-main/checkpoint-arrays/Data_Structures_Arrays_1/src/minimumAndMaximum.js
@@ -1,11 +1,25 @@
 /**
  * Implement an algorithm that sorts the array then returns the minimum and maximum
  */
-function minimumAndMaximumSort(numbers) {}
-
-/**
- * Implement an algorithm that uses iteration to find the minimum and maximum
- */
-function minimumAndMaximumIterate(numbers) {}
-
-module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
+ function minimumAndMaximumSort(numbers) {
+    if (numbers.length === 0) {
+      return [];
+    }
+    const sorted = numbers.sort(function(a, b){return a - b});
+    return [sorted[0], sorted[sorted.length-1]]  
+  }
+  
+  
+  /**
+   * Implement an algorithm that uses iteration to find the minimum and maximum
+   */
+  function minimumAndMaximumIterate(numbers) {
+    if (numbers.length === 0) {
+      return [];
+    }
+    const sorted = numbers.sort(function(a, b){return a - b});
+    return [sorted[0], sorted[sorted.length-1]]  
+  }
+  
+  module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
+  
--- module-data-structures-main/checkpoint-arrays/Data_Structures_Arrays_1/src/missingNumber.js
+++ module-data-structures-main/checkpoint-arrays/Data_Structures_Arrays_1/src/missingNumber.js
@@ -1,13 +1,37 @@
 /**
  * Implement a brute force algorithm for finding the missing number in an array
  */
- function missingNumberBruteForce(numbers) {}
- console.log("hello")
-
- /**
-  * Use an iterative  strategy for finding the missing number in an array
-  */
- function missingNumberSum(numbers) {}
- 
- module.exports = { missingNumberBruteForce, missingNumberSum };
- 
+ function missingNumberBruteForce(numbers) {
+    const N = numbers.length + 1;
+    for (let i = 1; i <= N; i++) {
+      let found = false;
+      let j = 0;
+      while (found === false && j < numbers.length) {
+        if (i === numbers[j]) {
+          found = true;
+        }
+        j++
+      }
+      if (found === false) {
+        return i;
+      }
+    }
+  }
+  
+  
+  /**
+   * Use an iterative  strategy for finding the missing number in an array
+   */
+  function missingNumberSum(numbers) {
+   const N = numbers.length + 1;
+   const fullSum = (N * (N + 1)) / 2;
+   let sum = 0;
+   for (let i = 0; i < numbers.length; i++) {
+     sum = sum + numbers[i];
+   }
+   return fullSum - sum
+  }
+  console.log(missingNumberSum([12, 13, 15]))
+  
+  module.exports = { missingNumberBruteForce, missingNumberSum };
+  
